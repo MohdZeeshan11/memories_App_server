054e80cc70b1971298d287e0b47b37301658cf7e
Mohd Zeeshan <100269606+MohdZeeshan11@users.noreply.github.com>
2023-01-13 11:13:08 +0530

    error fixed

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,29 +1,51 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
 
-const auth = async (req, res, next) => {
-  console.log("--------------------------",req.headers);
-  try {
-    const token = req?.headers?.authorization?.split(" ")[1];
-    const isCustomAuth = token?.length;
-    console.log('length =',token?.length);
-    let decodedData;
+// const auth = async (req, res, next) => {
+//   console.log("--------------------------",req.headers);
+//   try {
+//     const token = req?.headers?.authorization?.split(" ")[1];
+//     const isCustomAuth = token?.length;
+//     console.log('length =',token?.length);
+//     let decodedData;
 
-    if (token && isCustomAuth<500) {      
-      decodedData = jwt.verify(token, process.env.JWT_SIGNING_KEY);
+//     if (token && isCustomAuth<500) {      
+//       decodedData = jwt.verify(token, process.env.JWT_SIGNING_KEY);
+//       req.userId = decodedData?.id;
+//     } else {
+//       decodedData = jwt.decode(token);
 
-      req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
+//       req.userId = decodedData?.sub;
+//     }    
+//     next();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-      req.userId = decodedData?.sub;
-    }    
+// module.exports = auth;
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors/index')
 
-    next();
-  } catch (error) {
-    console.log(error);
+const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('No token provided')
   }
-};
 
-module.exports = auth;
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    // const { id, username } = decoded
+    // req.user = { id, username }
+    req.user = decoded
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Not authorized to access this route')
+  }
+}
+
+module.exports = authenticationMiddleware
 
